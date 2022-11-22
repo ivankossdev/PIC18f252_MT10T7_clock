729eb8278a04623d14466750860255adc39fba3f
Ivan <98459118+ivankossdev@users.noreply.github.com>
2022-11-22 12:22:55 +0300

    Update МТ10Т7.h

--- МТ10Т7.h
+++ МТ10Т7.h
@@ -1,14 +1,21 @@
-#ifndef MT10T7_H
+#ifndef MT107T_I2C_H
+#define	MT107T_I2C_H
 #include <xc.h>
-#define _XTAL_FREQ 4000000
+#include "i2c.h"
 
-char L_Half_Byte(char b);
-void Send_Data(char adr, char L_Data, char H_Data);
-char LCD_dig_conv(char digit);
-void LCD_Send_Char(char adress, char conv, char pnt);
-void LCD_clear();
-void LCD_seg_conv(unsigned int S);
-void LCD_print_segment(unsigned int seg, unsigned char pos);
-void LCD_scroll(unsigned int seg, char pos);
-void LCD_Unlock();
-#endif
+#define _XTAL_FREQ 4000000
+#define A0_ON      I2C_SendByteByADDR(buffer |= 0b00000001, 0x70)
+#define A0_OFF     I2C_SendByteByADDR(buffer &= 0b11111100, 0x70) 
+#define WR1_ON     I2C_SendByteByADDR(buffer |= 0b00000100, 0x70)
+#define WR1_OFF    I2C_SendByteByADDR(buffer &= 0b11111001, 0x70)
+#define CLR_buffer I2C_SendByteByADDR(buffer &= 0b00000111, 0x70)
+void I2C_Send_Data(char adr, char L_Data, char H_Data);
+void I2C_LCD_Clear(void);
+void I2C_LCD_Send_Char(char adress, char conv, char pnt);
+void I2C_LCD_seg_conv(unsigned int h, unsigned int m, unsigned int s);
+void I2C_LCD_print_time(void);
+void I2C_LCD_set_print_time(char point);
+void I2C_LCD_temp(unsigned int TM, unsigned int TL);
+void I2C_LCD_print_temp(void);
+#endif	/* MT107T_I2C_H */
+
